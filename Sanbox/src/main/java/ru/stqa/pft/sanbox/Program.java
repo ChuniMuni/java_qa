7bd8adde85d5bd0faf1a327db4b519322ca35c11
ChuniMuni <tstvitali@gmail.com>
2020-02-18 22:19:14 +0300

    Triangle quest added;)

--- Sanbox/src/main/java/ru/stqa/pft/sanbox/Program.java
+++ Sanbox/src/main/java/ru/stqa/pft/sanbox/Program.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.sanbox;
 
+import java.util.Scanner;
+
 public class Program {
 	public static void main(String[] args){
 		hello("world");
@@ -14,6 +16,16 @@ public class Program {
 		Point p1 = new Point(-4,10);
 		Point p2 = new Point(0,13);
 		System.out.println("Расстояние между точками (" + p1.toString() + ") и (" + p2.toString() + ") = " + p1.distance(p2));
+
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+
+		if (a + b > c && a + c > b && b + c > a)
+			System.out.println("Треугольник существует.");
+		else
+			System.out.println("Треугольник не существует.");
 	}
 
 	public static void hello(String somebody) {
