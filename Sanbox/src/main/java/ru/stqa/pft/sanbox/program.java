3c34e56318fdbb6b0e235169bc70d703d34d397e
ChuniMuni <tstvitali@gmail.com>
2020-02-18 19:33:08 +0300

    Adding classes and constructors.

--- Sanbox/src/main/java/ru/stqa/pft/sanbox/program.java
+++ Sanbox/src/main/java/ru/stqa/pft/sanbox/program.java
@@ -4,22 +4,23 @@ public class program{
 	public static void main(String[] args){
 		hello("world");
 		hello("user");
-		double len = 7;
-		System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
-		double a = 7;
-		double b = 5;
-		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+
+		Square s = new Square(5);
+		System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+
+		Rectangle r = new Rectangle(7, 5);
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
 	}
 
 	public static void hello(String somebody) {
 		System.out.println("Hello, " + somebody + "!");
 	}
 
-	public static double area(double l) {
-		return l * l;
+	public static double area(Square s) {
+		return s.l * s.l;
 	}
 
-	public static double area(double a, double b) {
-		return a * b;
+	public static double area(Rectangle r) {
+		return r.a * r.b;
 	}
 }
