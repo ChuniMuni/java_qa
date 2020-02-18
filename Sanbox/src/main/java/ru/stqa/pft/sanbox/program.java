7eae53ea3ab397fe3acb821e9f2ef0bb3373c990
ChuniMuni <tstvitali@gmail.com>
2020-02-18 18:45:27 +0300

    Added several methods.

--- Sanbox/src/main/java/ru/stqa/pft/sanbox/program.java
+++ Sanbox/src/main/java/ru/stqa/pft/sanbox/program.java
@@ -2,6 +2,24 @@ package ru.stqa.pft.sanbox;
 
 public class program{
 	public static void main(String[] args){
-		System.out.println("Hello, world!");
+		hello("world");
+		hello("user");
+		double len = 7;
+		System.out.println("Сторона квадрата со стороной " + len + " = " + area(len));
+		double a = 7;
+		double b = 5;
+		System.out.println("Сторона прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+	}
+
+	public static void hello(String somebody) {
+		System.out.println("Hello, " + somebody + "!");
+	}
+
+	public static double area(double l) {
+		return l * l;
+	}
+
+	public static double area(double a, double b) {
+		return a * b;
 	}
 }
