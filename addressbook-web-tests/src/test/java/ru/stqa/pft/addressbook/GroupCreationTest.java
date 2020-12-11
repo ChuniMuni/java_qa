796ae776d8e81460c2ab4c8e9ea550c07e0746d9
ChuniMuni <tstvitali@gmail.com>
2020-12-11 16:54:50 +0300

    Refactoring test fixtures

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTest.java
@@ -3,9 +3,11 @@ package ru.stqa.pft.addressbook;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.IsNot.not;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -20,6 +22,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.Keys;
+
 import java.util.*;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -27,18 +30,12 @@ import java.net.URL;
 public class GroupCreationTest {
   private WebDriver driver;
   JavascriptExecutor js;
+
   @Before
   public void setUp() {
     driver = new FirefoxDriver();
     js = (JavascriptExecutor) driver;
     Map<String, Object> vars = new HashMap<String, Object>();
-  }
-  @After
-  public void tearDown() {
-    driver.quit();
-  }
-  @Test
-  public void addressbook() {
     driver.get("http://localhost/addressbook/");
     driver.manage().window().setSize(new Dimension(1550, 838));
     driver.findElement(By.name("user")).click();
@@ -47,6 +44,10 @@ public class GroupCreationTest {
     driver.findElement(By.name("pass")).click();
     driver.findElement(By.name("pass")).sendKeys("secret");
     driver.findElement(By.cssSelector("input:nth-child(7)")).click();
+  }
+
+  @Test
+  public void addressbook() {
     driver.findElement(By.linkText("groups")).click();
     driver.findElement(By.name("new")).click();
     driver.findElement(By.name("group_name")).click();
@@ -57,5 +58,10 @@ public class GroupCreationTest {
     driver.findElement(By.name("group_footer")).sendKeys("test2");
     driver.findElement(By.name("submit")).click();
     driver.findElement(By.linkText("group page")).click();
+  }
+
+  @After
+  public void tearDown() {
+    driver.quit();
   }
 }
