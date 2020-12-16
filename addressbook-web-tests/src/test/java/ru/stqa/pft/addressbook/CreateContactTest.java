ca7c65b96c17aba873929e78b22f0016227e5dd7
ChuniMuni <tstvitali@gmail.com>
2020-12-16 18:40:18 +0300

    Disabled Kotlin

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/CreateContactTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/CreateContactTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class CreateContactTest extends TestBase {
 
   @Test
-  public void createcontact() {
+  public void createContact() {
     gotoNewContactPage();
     addNewContact(new ContactData("Ivan", "Ivanovich", "Ivanov", "ChuniMuni", "User", "Home", "Nignie Vasuki", "22322332", "+7809332222", "+4959993322", "+4959992233", "test@gmail.com", "test1@gmail.com", "test2@gmail.com", "http://localhost/addressbook/", "Verhnie Vasuki", "Home", "Test"));
     createNewContact();
