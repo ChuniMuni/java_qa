3dddc45d589024f7472593acd87fc2992bed399e
ChuniMuni <tstvitali@gmail.com>
2021-02-03 16:52:29 +0300

    Add modification group test.

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.addressbook.tests;
+
+
+import org.junit.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupModificationTest extends TestBase {
+
+    @Test
+    public void testGroupModification() {
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("modifi", "modifi1", "modifi2"));
+        app.getGroupHelper().submitGroupModification();
+        app.returnGroups();
+    }
+}
